37229b76c50d66e20c9d28c19dad89ac49cf325a
TU NGUYEN <trongtunguyen08@icloud.com>
2024-06-27 11:11:46 +0700

    Initial Commit

---
+++ AppIdeas/AppIdeasApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppIdeasApp.swift
+//  AppIdeas
+//
+//  Created by Tu Nguyen on 27/6/24.
+//
+
+import SwiftUI
+
+@main
+struct AppIdeasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
